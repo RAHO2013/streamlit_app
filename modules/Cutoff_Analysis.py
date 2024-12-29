fae90b1896fb19fa7517848a54eaad30153d7134
RAHO2013 <myranktech.ppt@gmail.com>
2024-12-29 11:04:43 +0530

    Update and rename Cutoff_Analysis.py to cutoff_Analysis.py

--- modules/Cutoff_Analysis.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def display_fee_checking():
-    st.title("Fee Checking")
-    st.write("Fee checking functionality coming soon.")
---
+++ modules/cutoff_Analysis.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def display_Cutoff_Analysis():
+    st.title("Cutoff Analysis")
+    st.write("Cutoff Analysis functionality coming soon.")
